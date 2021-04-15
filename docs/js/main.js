2471a867a94e2c36e0f749ac5b22dcdaed842bef
seraphinush-gaming <seraphinush@gmail.com>
2021-04-15 14:32:43 -0700

    Added v105.02.01 patch notes

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,8 @@
 window.addEventListener('load', function () {
-  // helper
-  //const sleep = (ms) => { return new Promise((resolve) => { setTimeout(resolve, ms); }); }
-
   // sidebar toggle
   const burgers = document.querySelector('.burgers');
   const navMobile = document.querySelector('.nav-mobile');
+
   let menuOpen = false;
   burgers.addEventListener('click', () => {
     if (menuOpen) {
--- docs/js/modal.js
+++ docs/js/modal.js
@@ -1,27 +1,63 @@
 window.addEventListener('load', function () {
-  const modal_image = document.getElementById('modal-image');
-  const modal_container = document.getElementById('modal-container');
-  const modal_close = document.getElementById('modal-close');
+  const modalImage = document.getElementById('modal-image');
+  const modalContainer = document.getElementById('modal-container');
+  const modalCloseButton = document.getElementById('modal-close-button');
 
-  const animation_duration = '0.6s';
-  modal_image.style['animation-duration'] = animation_duration;
-  modal_image.style['-webkit-animation-duration'] = animation_duration;
+  const animationDuration = '0.6s';
+  modalImage.style['animation-duration'] = animationDuration;
+  modalImage.style['-webkit-animation-duration'] = animationDuration;
 
-  modal_close.onclick = function () {
-    modal_image.style['animation-name'] = 'zoom-out';
-    modal_image.style['-webkit-animation-name'] = 'zoom-out';
+  const burgers = document.querySelector('.burgers');
+
+  modalCloseButton.addEventListener('click', () => { closeModal(); });
+
+  closeModal = () => {
+    modalImage.style['animation-name'] = 'zoom-out';
+    modalImage.style['-webkit-animation-name'] = 'zoom-out';
     setTimeout(function () {
-      modal_container.style.display = 'none';
-    }, parseFloat(animation_duration) * 1000);
+      modalContainer.style.display = 'none';
+      burgers.style.display = '';
+      modalActive = false;
+    }, parseFloat(animationDuration) * 1000);
   }
+
+  let modalActive = false;
 
   document.addEventListener("click", function (e) {
     if (e.target.classList.contains('nav-logo')) return;
     if (e.target.localName == 'img') {
-      modal_image.style['animation-name'] = 'zoom-in';
-      modal_image.style['-webkit-animation-name'] = 'zoom-in';
-      modal_image.src = e.target.src;
-      modal_container.style.display = 'block';
+      modalImage.style['animation-name'] = 'zoom-in';
+      modalImage.style['-webkit-animation-name'] = 'zoom-in';
+      modalImage.src = e.target.src;
+      modalContainer.style.display = 'block';
+      burgers.style.display = 'none';
+      modalActive = true;
     }
-  })
+  });
+
+  // mobile events
+  let initialTouchY = 0;
+  let screenHeight = window.screen.height
+  prepareCloseModal = false;
+  document.addEventListener("touchstart", function (e) {
+    if (!modalActive) return;
+    initialTouchY = e.touches[e.touches.length - 1].screenY;
+  });
+
+  document.addEventListener("touchmove", function (e) {
+    if (!modalActive) return;
+    let currentTouchY = e.touches[e.touches.length - 1].screenY;
+    if ((currentTouchY - initialTouchY) > (screenHeight * 0.4)) {
+      prepareCloseModal = true;
+    }
+  });
+
+  document.addEventListener("touchend", function (e) {
+    if (!modalActive) return;
+    if (prepareCloseModal) {
+      closeModal();
+      prepareCloseModal = false;
+    }
+  });
+
 });
